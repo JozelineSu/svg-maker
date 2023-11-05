a93efd2a713ac45c8385b16b99f474131face09a
unknown <jozelinesuarez@gmail.com>
2023-11-05 13:53:37 -0600

    Did code for circle shape + test for circle

--- index.js
+++ index.js
@@ -1,27 +1,48 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+const Circle = require('./lib/circle');
 
-const shapes = ['Circle', 'Trianlge', 'Square'];
+function makeSVG(answer) {
+    if (answer.shape === 'Circle') {
+        let svg = new Circle (answer.shapeColor, answer.text, answer.textColor)
+        return svg.render()
+    }
+}
+
+const shapeList = ['Circle', 'Triangle', 'Square'];
 
 inquirer.prompt([
     {
         type: 'input',
         name: 'text',
         message: 'Enter up to 3 characters:',
+        validate(input) {
+            if(input.length != 3) {
+                return 'Please enter 3 characters'
+            } 
+            return true
+        }
     },
       {
         type: 'input',
-        name: 'text-color',
+        name: 'textColor',
         message: 'What color do you want your text? (Enter color keyword OR hexadecimal number)',
     },
     {
         type: 'list',
-        name: 'shapes',
+        name: 'shape',
         message: 'Choose a shape:',
-        choices: shapes,
+        choices: shapeList,
     },
       {
         type: 'input',
-        name: 'shape-color',
+        name: 'shapeColor',
         message: 'What color do you want your shape? (Enter color keyword OR hexadecimal number)',
     },  
-])
+]) .then((answer) => {
+    const svg = makeSVG(answer);
+    fs.writeFile('./examples/logo.svg', svg, ()=> console.log('Made logo'));
+    
+}).catch(err => {
+    console.log(err)
+});
